c7f198fd7f6be83ac0f1007e2e6af37c1841a43e
Krish Kumar <krishkr372@gmail.com>
2026-09-17 22:17:52 +0530

    new training script

--- SLI/train.py
+++ SLI/train.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import joblib
 
-#Load the data from the csv file
+#Load the data from the csv file. You have to download the csv file from the link provided in the README.md file and place it in the same directory as this script.
 df = pd.read_csv("sli_hand_landmarks.csv")
 X = df.drop('label', axis=1)
 y = df['label']
@@ -13,10 +13,17 @@ y = df['label']
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
+# Train/test split
+x_train, x_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
 # Train the model
-model = RandomForestClassifier(n_estimators=150, random_state=42)
-model.fit(X_scaled, y)
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(x_train, y_train)
+
+# Evaluate the model
+accuracy = model.score(x_test, y_test)
+print(f"Model Accuracy: {accuracy:.2f}")
 
 # Save the model
-joblib.dump(model, "sli_model.pkl")
-joblib.dump(scaler, "sli_scaler.pkl")
+joblib.dump(model, "sli_asl_model.pkl")
+joblib.dump(scaler, "sli_asl_scaler.pkl")
